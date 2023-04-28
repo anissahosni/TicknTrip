c877ab452dc2d4f169f1b446acfca075b8968131
Julien Cévaër <julien.cevaer@doinsport.com>
2023-04-28 14:32:02 +0200

    implemented checklist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tickntrip';
+  title = 'my-app';
+
+  trips = [
+    {
+      startDate: '2023-05-22',
+      endDate: '2023-05-25',
+      city: 'Lisbon',
+      checked: false,
+      items: []
+    },
+    {
+      startDate: '2023-07-01',
+      endDate: '2023-07-08',
+      city: 'New York',
+      checked: false,
+      items: [{ name: 'Chemises/Blouses (3)', checked: false }, { name: 'Pantalons/Shorts (2)', checked: false }, { name: 'Robes/Jupes (2)', checked: false }, { name: 'Pulls/Gilets (2)', checked: false }, { name: 'Veste légère (1)', checked: false }, { name: 'Maillots de bain (2)', checked: false }, { name: 'Sous-vêtements (suffisamment pour la durée du voyage)', checked: false }, { name: 'Chaussettes (suffisamment pour la durée du voyage)', checked: false }, { name: 'Chaussures confortables pour la marche (1 paire)', checked: false }, { name: 'Chaussures de soirée (1 paire)', checked: false }, { name: 'Sandales/Tongs (1 paire)', checked: false }, { name: 'Chapeau/casquette', checked: false }
+      ]
+    },
+    {
+      startDate: '2023-07-22',
+      endDate: '2023-07-29',
+      city: 'Paris',
+      checked: false,
+      items: []
+    }
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  prepareSuitcase(trip: { startDate: any; endDate: any; city: any }) {
+    const url = `/generate`;
+    const body = { departureDate: trip.startDate, arrivalDate: trip.endDate, location: trip.city };
+    this.http.post(url, body).subscribe(
+      (response) => {
+        console.log('RESPONSE: ', response)
+        // const items = response.items;
+        // items.forEach(item => {
+        //   item.checked = false;
+        // });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
